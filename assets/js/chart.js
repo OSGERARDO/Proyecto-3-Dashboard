7eb6476ce88e7c40bfc737ed63e428702f5f9a0f
OSGERARDO <nmsosx@gmail.com>
2023-07-27 23:30:16 -0400

    se agrega grafico

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,21 +1,29 @@
 
 const ctx = document.getElementById('myChart');
-  
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+let grafico;
+
+export const crearGrafico = (fechas,temperaturas) => {
+
+if (grafico) {
+  grafico.destroy()
+}
+
+  grafico = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: fechas.splice(0,16),
+      datasets: [{
+        label: 'Tº Próximos 2 días',
+        data: temperaturas.splice(0,16),
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  }
-});
+  });
+}
